cffe500958ec52bf0b396ffb61ce7926abeb7fd1
Gabriel Celery <gabriel.celery@autologyx.com>
2019-03-05 12:39:20 +0000

    - Synchronized navbar and page routing/rendering to use the same variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,22 +10,62 @@ import Invoices from './pages/Invoices';
 import Statistics from './pages/Statistics';
 import Settings from './pages/Settings';
 
+const Routes = {
+  Dashboard,
+  Contracts,
+  Mailing,
+  Invoices,
+  Statistics,
+  Settings
+}
+
 class App extends Component {
   render() {
+    const pageConfigs = [{
+      label: 'Dasboard',
+      icon: 'tachometer-alt',
+      path: '/dashboard',
+      page: 'Dashboard'
+    }, {
+      label: 'Contracts',
+      icon: 'file-contract',
+      path: '/contracts',
+      page: 'Contracts'
+    }, {
+      label: 'Mailing',
+      icon: 'envelope-square',
+      path: '/mailing',
+      page: 'Mailing'
+    }, {
+      label: 'Invoices',
+      icon: 'file-invoice',
+      path: '/invoices',
+      page: 'Invoices'
+    }, {
+      label: 'Statistics',
+      icon: 'chart-line',
+      path: '/statistics',
+      page: 'Statistics'
+    }, {
+      label: 'Settings',
+      icon: 'cog',
+      path: '/settings',
+      page: 'Settings'
+    }];
+
+    const renderedPages = pageConfigs.map((pageConfig, index) => (
+      <Route key={'page-' + index} path={pageConfig.path} component={Routes[pageConfig.page]} />
+    ));
+
     return (
       <Router>
         <React.Fragment>
           <div className="d-flex flex-row">
             <div className="w-10">
-              <Navbar />
+              <Navbar pageConfigs={pageConfigs} />
             </div>
             <div className="w-90">
-              <Route path="/dashboard" component={Dashboard} />
-              <Route path="/contracts" component={Contracts} />
-              <Route path="/mailing" component={Mailing} />
-              <Route path="/invoices" component={Invoices} />
-              <Route path="/statistics" component={Statistics} />
-              <Route path="/settings" component={Settings} />
+              {renderedPages}
             </div>
           </div>
         </React.Fragment>
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,41 +4,15 @@ import NavbarItem from './NavbarItem';
 
 export class Navbar extends React.Component {
 	render() {
-		const navbarItems = [{
-			label: 'Dasboard',
-			icon: 'tachometer-alt',
-			path: '/dashboard'
-		}, {
-			label: 'Contracts',
-			icon: 'file-contract',
-			path: '/contracts'
-		}, {
-			label: 'Mailing',
-			icon: 'envelope-square',
-			path: '/mailing'
-		}, {
-			label: 'Invoices',
-			icon: 'file-invoice',
-			path: '/invoices'
-		}, {
-			label: 'Statistics',
-			icon: 'chart-line',
-			path: '/statistics'
-		}, {
-			label: 'Settings',
-			icon: 'cog',
-			path: '/settings'
-		}];
-
-		const renderedNavbarItems = navbarItems.map((navbarItem, index) => (
-			<NavbarItem key={'nav-item-' + index} label={navbarItem.label} icon={navbarItem.icon} path={navbarItem.path}/>
+		const renderedNavbarItems = this.props.pageConfigs.map((pageConfig, index) => (
+			<NavbarItem key={'nav-item-' + index} label={pageConfig.label} icon={pageConfig.icon} path={pageConfig.path} />
 		));
 
 		return (
 			<React.Fragment>
-					<ul className="Navbar nav flex-column bg-gradient-primary">
-						{renderedNavbarItems}
-					</ul>
+				<ul className="Navbar nav flex-column bg-gradient-primary">
+					{renderedNavbarItems}
+				</ul>
 			</React.Fragment>
 		);
 	}
