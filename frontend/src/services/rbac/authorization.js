0ae4802278a6e68b2a74cbc5f48268a7340d5935
gabrieldcelery <Gabriel.d.celery@gmail.com>
2019-03-10 21:16:40 +0000

    - Added auth method to authorization

--- frontend/src/services/rbac/authorization.js
+++ frontend/src/services/rbac/authorization.js
@@ -1,6 +1,9 @@
 class Authorization {
-  isAuthorized () {
-    
+  isAuthorized(_rule) {
+    const _user = JSON.parse(localStorage.getItem('user')) || {};
+    const _userRoles = _user.roles || [];
+
+    return _userRoles.includes(_rule);
   }
 }
 
