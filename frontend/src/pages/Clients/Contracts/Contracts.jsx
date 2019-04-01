bcac6c076cee972973c463713cf5501b9602a55a
Gabriel Celery <gabriel.celery@autologyx.com>
2019-04-01 16:56:42 +0100

    - Playing around with datepicker react plugin -

--- frontend/src/pages/Clients/Contracts/Contracts.jsx
+++ frontend/src/pages/Clients/Contracts/Contracts.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { capitalizedLabels } from 'state/selectors';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { ContractsTable } from './ContractsTable';
 
 class Contracts extends Component {
   render() {
@@ -9,65 +12,51 @@ class Contracts extends Component {
         <div className="container mw-100">
           <div className="row">
             <div className="col-md-9">
-
-              <div className="card shadow-sm">
-                <div className="card-header text-center text-light bg-teal-gradient border-bottom-5 border-black">
-                  <h5>Contracts</h5>
-                </div>
-
-                <div className="card-body border-bottom-2">
-                  <table className="table table-sm table-striped">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-
-                <div className="card-footer">
-                  <ul className="pagination justify-content-end">
-                    <li className="page-item disabled">
-                      <a className="page-link" href="#" tabIndex="-1">Previous</a>
-                    </li>
-                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    <li className="page-item">
-                      <a className="page-link" href="#">Next</a>
-                    </li>
-                  </ul>
-                </div>
-
-              </div>
-
-
-
+              <ContractsTable />
             </div>
 
             <div className="col-md-3">
+
+              <div className="card shadow-sm mb-2">
+                <div className="card-header text-center text-light bg-teal-gradient border-bottom-5 border-black">
+                  Search
+                </div>
+                <div className="card-body">
+
+                  <div className="form-group">
+                    <label for="clientName">Client Name</label>
+                    <input type="text" className="form-control form-control-sm" id="clientName" />
+                  </div>
+
+                  <div className="form-group">
+                    <label for="clientSignatoryName">Client Signatory Name</label>
+                    <input type="text" className="form-control form-control-sm" id="clientSignatoryName" />
+                  </div>
+
+                  <div className="form-group">
+                    <label for="contractExpiryFrom">Contract Expiry From</label>
+                    <DatePicker id="contractExpiryFrom" className='form-control' />
+                  </div>
+
+                  <div className="form-group">
+                    <label for="contractExpiryTill">Contract Expiry Till</label>
+                    <DatePicker id="contractExpiryTill" className='form-control' />
+                  </div>
+
+                  <div className="form-group">
+                    <label for="bContractStatus">Contract Status</label>
+                    <select multiple className="form-control form-control-sm" id="bContractStatus">
+                      <option>Live</option>
+                      <option>Terminated</option>
+                    </select>
+                  </div>
+
+
+
+                  <button type="button" className="btn btn-primary btn-block">Search</button>
+                </div>
+              </div>
+
               <div className="card shadow-sm mb-2">
                 <div className="card-header text-center text-light bg-teal-gradient border-bottom-5 border-black">
                   Saved Views
@@ -83,17 +72,12 @@ class Contracts extends Component {
                     </select>
                   </div>
                   <button type="button" className="btn btn-primary btn-block">Remove View</button>
-                  <button type="button" className="btn btn-primary btn-block">Save View</button>
+                  <hr />
+                  <div className="form-group">
+                    <input type="text" className="form-control" id="" />
+                  </div>
+                  <button type="button" className="btn btn-primary btn-block">Save New View</button>
                 </div>
-              </div>
-
-              <div className="card shadow-sm mb-2">
-                <div className="card-header text-center text-light bg-teal-gradient border-bottom-5 border-black">
-                  Search
-                </div>
-                <div className="card-body">
-                  test
-								</div>
               </div>
 
               <div className="card shadow-sm mb-2">
