26ebbd9a5aceb940adb02bfcd400ae6b8c349d0d
gabrieldcelery <Gabriel.d.celery@gmail.com>
2019-04-02 23:17:11 +0100

    - Removed redundant code from quick search

--- frontend/src/pages/Clients/QuickSearch/QuickSearch.jsx
+++ frontend/src/pages/Clients/QuickSearch/QuickSearch.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { QuickSearchTable } from './QuickSearchTable';
-import { QuickSearchSidebar } from './QuickSearchSidebar';
 import { OneColMain } from 'components';
 
 export class QuickSearch extends Component {
 	render() {
 		return (
-			<React.Fragment>
 				<OneColMain
 					Content={QuickSearchTable}
 				/>
-			</React.Fragment>
 		);
 	}
 }
